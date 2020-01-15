98f824908746c81941b34ad64f78def91d28cfe0
NobodyXu <Jiahao_XU@outlook.com>
2020-01-15 13:51:57 +1100

    Create solution1.cc

---
+++ Solutions/reverse-linked-lists-ii/solution1.cc
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        ListNode *start = head;
+        ListNode **before_start = &start;
+        
+        for (int i = 1; i < m; ++i)
+            before_start = &(*before_start)->next;
+        
+        ListNode *new_last_node = *before_start;
+        
+        ListNode *prev = *before_start;
+        ListNode *curr = prev->next;
+        
+        for (int i = m; i != n; ++i) {
+            auto next = curr->next;
+            curr->next = prev;
+            
+            // Increment
+            prev = curr;
+            curr = next;
+        }
+        
+        *before_start = prev;
+        new_last_node->next = curr;
+        
+        return start;
+    }
+};
