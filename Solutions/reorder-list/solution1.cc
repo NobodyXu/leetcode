276dfd064336ff3ba69d40e4810941e9666339a5
NobodyXu <Jiahao_XU@outlook.com>
2020-01-18 11:07:53 +1100

    Second pre submission for reorder list.

--- Solutions/reorder-list/solution1.cc
+++ Solutions/reorder-list/solution1.cc
@@ -25,7 +25,7 @@ auto split_in_half(ListNode *head, std::size_t len) -> std::pair<ListNode*, List
     ListNode *first_odd_part = head;
     ListNode *last_odd_part = head;
     
-    auto odd_part_len = len >> 1 + (len & 1); // len / 2 + len % 2
+    auto odd_part_len = (len >> 1) + (len & 1); // len / 2 + len % 2
     for (std::size_t i = 0; i != (odd_part_len - 1); ++i)
         last_odd_part = last_odd_part->next;
     
@@ -36,29 +36,27 @@ auto split_in_half(ListNode *head, std::size_t len) -> std::pair<ListNode*, List
 }
 
 /**
- * @lists (odd_part, even_part)
- * 
  * Precondition: odd_part, even_part != nullptr
  */
-void merge(std::pair<ListNode*, ListNode*> lists) {
-    auto prev_odd = lists.first;
-    auto curr_even = lists.second;
+void merge(ListNode *odd_part, ListNode *even_part) {
+    auto prev_odd = odd_part;
+    auto curr_even = even_part;
     
     /*
-     * If len of the original list is even, then
+     * if len of the original list is even, then
      * 
      *     the two part is of the same size.
      *     Because the odd part is forwarded once before the loop, so the odd part should reach
      *     the nullptr faster than the even part.
      *
-     * Else, then
+     * else, then
      * 
      *     the size of odd part = the size of even part + 1.
      *     Due to the forward of odd before the loop, 
      *     the size of odd part = the size of even part at the beginning of the loop.
      *     So they shall reach the end at the same iteration.
      * 
-     * Fi
+     * fi
      * 
      * So there is not need for testing the end condition of the 2 parts, only the odd part will suffice.
      */
@@ -76,8 +74,12 @@ void merge(std::pair<ListNode*, ListNode*> lists) {
     }
     
     // Since either both parts meets the end condition or only the odd part meets,
-    // let's put the rest nodes of the even part after the last node of the curr part.
+    // let's put the rest nodes of the even part after the last node of the curr part.t
     prev_odd->next = curr_even;
+}
+
+auto reverse(ListNode *head) -> ListNode* {
+    ;
 }
 
 /**
@@ -91,7 +93,9 @@ void merge(std::pair<ListNode*, ListNode*> lists) {
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        if (head != nullptr && head->next != nullptr && head->next->next != nullptr)
-            return merge(split_in_half(head, size(head)));
+        if (head != nullptr && head->next != nullptr && head->next->next != nullptr) {
+            auto [odd_part, even_part] = split_in_half(head, size(head));
+            return merge(odd_part, reverse(even_part));
+        }
     }
 };
