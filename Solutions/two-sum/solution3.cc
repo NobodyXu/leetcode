f802a1184acf60065a33c426ed749ec4851c9de9
NobodyXu <Jiahao_XU@outlook.com>
2020-01-26 13:54:02 +1100

    Create solution3.cc

---
+++ Solutions/two-sum/solution3.cc
@@ -0,0 +1,41 @@
+#include <type_traits>
+#include <algorithm>
+
+template <class T>
+using rm_cvref = std::remove_cv_t<std::remove_reference_t<T>>;
+
+auto make_ref_vector(const vector<int> &nums) -> vector<typename vector<int>::const_iterator> {
+    vector<typename vector<int>::const_iterator> ret;
+    
+    ret.reserve(nums.size());
+    for (auto it = nums.begin(); it != nums.end(); ++it)
+        ret.push_back(it);
+    
+    return ret;
+}
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        auto ref_vector = make_ref_vector(nums);
+        
+        std::sort(ref_vector.begin(), ref_vector.end(), [](auto &it1, auto &it2) noexcept {
+            return *it1 < *it2;
+        });
+        
+        for (auto it = nums.cbegin(); it != nums.cend(); ++it) {
+            int x = *it;
+            int y = target - x;
+            
+            // **result >= y
+            auto result = std::lower_bound(ref_vector.cbegin(), ref_vector.cend(), y, [](const auto &it, const auto &y) noexcept {
+                    return *it < y;
+            });
+            if (result != ref_vector.cend() && **result == y && *result != it)
+                return {it - nums.cbegin(), *result - nums.cbegin()};
+        }
+        
+        // Dummy return statements just to suppress the warning.
+        return {};
+    }
+};
