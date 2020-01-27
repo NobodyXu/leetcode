199ee6cfa6e38cc3f31555cc0b037d3f2fdb176a
NobodyXu <Jiahao_XU@outlook.com>
2020-01-27 16:44:49 +1100

    Fix typos and improve readability
    Improve readability by changing const iterator name from `it*` to `cit*`.

--- Solutions/three-sum/solution1.cc
+++ Solutions/three-sum/solution1.cc
@@ -14,7 +14,7 @@ auto make_ref_vector(const vector<int> &nums) -> ref_vector_t {
     for (auto it = nums.begin(); it != nums.end(); ++it)
         ret.push_back(it);
     
-    std::sort(ref_vector.begin(), ref_vector.end(), [](auto &it1, auto &it2) noexcept {
+    std::sort(ret.begin(), ret.end(), [](auto &it1, auto &it2) noexcept {
         return *it1 < *it2;
     });
 
@@ -31,23 +31,23 @@ auto sorted(int x, int y, int z) -> triplet {
     return {s1, s2, s3};
 }
 
-auto threeSum(const ref_vector_t &ref_vector) -> std::set<triplet> {
+auto threeSum_set(const ref_vector_t &ref_vector) -> std::set<triplet> {
     std::set<triplet> ret;
     
     for (auto &cit1: ref_vector)
         for (auto &cit2: ref_vector)
-            if (it1 != it2) {
-                int x = *it1;
-                int y = *it2;
+            if (cit1 != cit2) {
+                int x = *cit1;
+                int y = *cit2;
                 int z = 0 - x - y;
                 
-                auto ref_it3 = std::lower_bound(ref_vector.begin(), ref_vector.end(), z, 
-                                                [](const auto &it, const auto &z) noexcept {
-                    return *it < z;
+                auto ref_cit3 = std::lower_bound(ref_vector.begin(), ref_vector.end(), z, 
+                                                [](const auto &cit, const auto &z) noexcept {
+                    return *cit < z;
                 });
                 
-                if (ref_it3 != ref_vector.end() && *(*ref_it3) == z && 
-                    *ref_it3 != it1 && *ref_it3 != it2)
+                if (ref_cit3 != ref_vector.end() && *(*ref_cit3) == z && 
+                    *ref_cit3 != cit1 && *ref_cit3 != cit2)
                     ret.insert(sorted(x, y, z));
             }
     
@@ -57,7 +57,7 @@ auto threeSum(const ref_vector_t &ref_vector) -> std::set<triplet> {
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        auto set = threeSum(make_ref_vector(nums));
+        auto set = threeSum_set(make_ref_vector(nums));
         
         vector<triplet> ret;
         ret.reserve(set.size());
