7adbc57d9b73cc4452dda9d66243eb1a83fc4034
NobodyXu <Jiahao_XU@outlook.com>
2020-01-27 21:02:04 +1100

    Add solution2.cc for 3sum
    This solution utilize std::unordered_map to speed up the code.

---
+++ Solutions/3sum/solution2.cc
@@ -0,0 +1,100 @@
+#include <cstdint>
+#include <cassert>
+#include <utility>
+
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+
+using triplet = vector<int>;
+using dict_t = std::unordered_map<int, unsigned>;
+
+auto to_dict(const vector<int> &nums) -> dict_t {
+    dict_t dict;
+    
+    dict.reserve(nums.size());
+    for (int each: nums)
+        ++dict[each];
+    
+     return dict;
+}
+
+/**
+ * @Return x, y, z in ascending order as type triplet
+ */
+auto sorted(int x, int y, int z) -> triplet {    
+    auto [s2, s3] = std::minmax(x, y);
+    auto [s4, s5] = std::minmax(s3, z);
+    
+    auto [s0, s1] = std::minmax(s2, s4);
+    
+    return {s0, s1, s5};
+}
+
+auto threeSum_impl(const dict_t &dict) -> std::set<triplet> {
+    std::set<triplet> ret;
+    
+    auto end = dict.end();
+    for (auto cit1 = dict.begin(); cit1 != end; ++cit1) {
+        auto cit2 = cit1;
+        
+        if (cit1->second == 1)
+            ++cit2;
+        
+        for (; cit2 != end; ++cit2) {
+            int x = cit1->first;
+            int y = cit2->first;
+            int z = 0 - (x + y);
+                
+            auto cit3 = dict.find(z);
+                
+            if (cit3 != end) {
+                switch (uint8_t{cit3 != cit2} << 2 |
+                        uint8_t{cit3 != cit1} << 1 | 
+                        uint8_t{cit2 != cit1}) {
+                    case 0:
+                        if (cit1->second < 3)
+                            continue;
+                        else
+                            break;
+                        
+                    case 2:
+                    case 4:
+                        assert(false);
+                        
+                    case 1:
+                    case 3:
+                        if (cit2->second == 1)
+                            continue;
+                        
+                    case 5:
+                    case 6:
+                        if (cit1->second == 1)
+                            continue;
+                        
+                    case 7:
+                        ;
+                }
+                
+                ret.insert(sorted(x, y, z));
+            }
+        }
+    }
+    
+    return ret;
+}
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        auto set = threeSum_impl(to_dict(nums));
+        
+        vector<triplet> ret;
+        ret.reserve(set.size());
+        
+        for (auto &each: set)
+            ret.push_back(std::move(each));
+        
+        return ret;
+    }
+};
