634e61063a8c8534ee7dffc500f20a37b13f31a8
NobodyXu <Jiahao_XU@outlook.com>
2020-01-27 18:06:10 +1100

    Remove commented code in sorted(int, int, int).

--- Solutions/three-sum/solution1.cc
+++ Solutions/three-sum/solution1.cc
@@ -10,15 +10,6 @@ using triplet = vector<int>;
  * @Return x, y, z in ascending order as type triplet
  */
 auto sorted(int x, int y, int z) -> triplet {
-    /*
-    triplet ret;
-    
-    ret.reserve(3);
-    ret.push_back(x);
-    ret.push_back(y);
-    ret.push_back(z);
-    */
-    
     auto [s2, s3] = std::minmax(x, y);
     auto [s4, s5] = std::minmax(s3, z);
     
