d67817a464b87a129544e4bdeb213f6f2f4925ae
NobodyXu <Jiahao_XU@outlook.com>
2020-01-27 18:58:24 +1100

    First working solution for three-sum!
    But it is really slow.

--- Solutions/three-sum/solution1.cc
+++ Solutions/three-sum/solution1.cc
@@ -1,4 +1,5 @@
 #include <utility>
+#include <iterator>
 
 #include <algorithm>
 #include <vector>
@@ -9,7 +10,7 @@ using triplet = vector<int>;
 /**
  * @Return x, y, z in ascending order as type triplet
  */
-auto sorted(int x, int y, int z) -> triplet {
+auto sorted(int x, int y, int z) -> triplet {    
     auto [s2, s3] = std::minmax(x, y);
     auto [s4, s5] = std::minmax(s3, z);
     
@@ -18,19 +19,27 @@ auto sorted(int x, int y, int z) -> triplet {
     return {s0, s1, s5};
 }
 
+template <class InputIt>
+auto skip_same(InputIt beg, InputIt end) {
+    auto &val = *beg;
+    return std::find_if(std::next(beg), end, [&val](auto &each) noexcept {
+        return each != val;
+    });
+}
+
 auto threeSum_set(const vector<int> &nums) -> std::set<triplet> {
     std::set<triplet> ret;
     
-    for (auto cit1 = nums.begin(); cit1 != nums.end(); ++cit1)
-        for (auto cit2 = cit1; ++cit2 != nums.end(); ) {
+    auto end = nums.end();
+    for (auto cit1 = nums.begin(); cit1 != end; cit1 = skip_same(cit1, end))
+        for (auto cit2 = std::next(cit1); cit2 != nums.end(); cit2 = skip_same(cit2, end)) {
             int x = *cit1;
             int y = *cit2;
             int z = 0 - (x + y);
                 
-            auto cit3 = std::lower_bound(nums.begin(), nums.end(), z);
+            auto cit3 = std::lower_bound(std::next(cit2), nums.end(), z);
                 
-            if (cit3 != nums.end() && *cit3 == z && 
-                cit3 != cit1 && cit3 != cit2)
+            if (cit3 != nums.end() && *cit3 == z)
                 ret.insert(sorted(x, y, z));
         }
     
