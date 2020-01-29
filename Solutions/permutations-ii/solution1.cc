cb951de0e702fcfa933d0772dc943c747b58cff0
NobodyXu <Jiahao_XU@outlook.com>
2020-01-29 13:57:27 +1100

    Copy the solution from permutations.

---
+++ Solutions/permutations-ii/solution1.cc
@@ -0,0 +1,17 @@
+#include <algorithm>
+
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ret;
+        ret.reserve(nums.size() * nums.size());
+        
+        std::sort(nums.begin(), nums.end());
+        
+        do {
+            ret.push_back(nums);
+        } while (std::next_permutation(nums.begin(), nums.end()));
+        
+        return ret;
+    }
+};
