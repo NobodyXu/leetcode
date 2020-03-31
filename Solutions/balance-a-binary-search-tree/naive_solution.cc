9401923bdfd8b27de81be50d19e80b4e4b042a95
nobodyxu <Jiahao_XU@outlook.com>
2020-04-01 00:39:46 +1100

    Fix boundary bug in sorted_to_balanced_BST
    This version has misterious failure output:
        AddressSanitizer:DEADLYSIGNAL
        =================================================================
        ==32==ERROR: AddressSanitizer: stack-overflow on address 0x7ffd9aa4bff8 (pc 0x0000003e9fd9 bp 0x7ffd9aa4c010 sp 0x7ffd9aa4c000 T0)
        ==32==ABORTING

--- Solutions/balance-a-binary-search-tree/naive_solution.cc
+++ Solutions/balance-a-binary-search-tree/naive_solution.cc
@@ -1,10 +1,16 @@
+/**
+ * AddressSanitizer:DEADLYSIGNAL
+ * =================================================================
+ * ==32==ERROR: AddressSanitizer: stack-overflow on address 0x7ffd9aa4bff8 (pc 0x0000003e9fd9 bp 0x7ffd9aa4c010 sp 0x7ffd9aa4c000 T0)
+ * ==32==ABORTING
+ */
 // Naive solution
 #include <stack>
 #include <algorithm>
 #include <iterator>
 
 // headers for assert_sorted
-#include <err.h>
+//#include <err.h>
 
 template <class T, class Container>
 auto pop(std::stack<T, Container> &s) {
@@ -46,6 +52,7 @@ auto BST_to_sorted(TreeNode *root) -> std::vector<TreeNode*> {
     } while (true);
 }
 
+/*
 auto& assert_sorted(const std::vector<TreeNode*> &v) {
     auto it = is_sorted_until(v.begin(), v.end(), [](const auto &x, const auto &y) {
         return x->val < y->val;
@@ -56,7 +63,7 @@ auto& assert_sorted(const std::vector<TreeNode*> &v) {
         err(1, "There are %zd elements unsorted, counted backwards", diff);
         
     return v;
-}
+}*/
 
 template <class T>
 constexpr bool is_odd(T val) {
@@ -78,19 +85,20 @@ auto* sorted_to_balanced_BST(const std::vector<TreeNode*> &v) {
     auto size = v.size();
     auto half = (size - 1) / 2;
     
-    for (; diff < half;) {
+    for (; diff <= half;) {
         for (auto i = start; i < (half * 2); i += pdiff * 2) {
             v[i]->left  = v[i - diff];
             v[i]->right = v[i + diff];
         }
+
         diff = pdiff;
         pdiff <<= 1;
         start += diff;
     }
-    
+
     if (is_even(size))
         v[size - 2]->right = v[size - 1];
-    
+
     return v[half];
 }
 
@@ -107,7 +115,7 @@ class Solution {
 public:
     TreeNode* balanceBST(TreeNode* root) {
         if (root != nullptr)
-            return sorted_to_balanced_BST(assert_sorted(BST_to_sorted(root)));
+            return sorted_to_balanced_BST(BST_to_sorted(root));
         else
             return nullptr;
     }
