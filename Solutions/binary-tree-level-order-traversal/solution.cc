023d0d42d3086ac3e5bdb73a4e275b54d61f86cc
nobodyxu <Jiahao_XU@outlook.com>
2020-04-06 23:38:55 +1000

    Add solution for binary-tree-level-order-traversal

---
+++ Solutions/binary-tree-level-order-traversal/solution.cc
@@ -0,0 +1,51 @@
+#include <utility>
+
+auto& push_nonnull(vector<TreeNode*> &v, TreeNode *node)
+{
+    if (node != nullptr)
+        v.push_back(node);
+    return v;
+}
+
+vector<vector<int>> levelOrder_impl(TreeNode *root)
+{
+    vector<vector<int>> ret;
+    
+    vector<TreeNode*> buffer;
+    vector<int> values;
+    vector<TreeNode*> next_buffer;
+    
+    buffer.push_back(root);
+    do {
+        values.reserve(buffer.size());
+        for (auto *node: buffer) {
+            values.push_back(node->val);
+            push_nonnull(push_nonnull(next_buffer, node->left), node->right);
+        }
+        
+        ret.push_back(std::move(values));
+        buffer = std::move(next_buffer);
+    } while (buffer.size() != 0);
+    
+    return ret;
+}
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root)
+    {
+        if (root != nullptr)
+            return levelOrder_impl(root);
+        else
+            return {};
+    }
+};
